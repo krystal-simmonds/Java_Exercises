0c583327a17536c00e5752a824edc74963f3df82
Krystal <krystalsimm01@me.com>
2020-08-11 00:00:42 +0100

    Showing results

---
+++ src/main/java/com/qa/day2/Results.java
@@ -0,0 +1,60 @@
+public class Results
+{
+    public static void main(String[] args)
+    {
+        Exam result = new Exam();
+        result.physics = 110;
+        result.chemistry = 130;
+        result.biology = 95;
+
+        System.out.println("Exam Marks");
+        System.out.println("-------------------------------------------------");
+        result.printMarksSummary();
+        System.out.println("-------------------------------------------------");
+    }
+}
+
+class Exam
+{
+    // Marks in various subjects
+    int physics;
+    int chemistry;
+    int biology;
+
+    int getPhysicsTotal() {
+        return physics;
+    }
+
+    int getChemistryTotal() {
+        return chemistry;
+    }
+
+    int getBiologyTotal() {
+        return biology;
+    }
+
+    int getAllSubjectsTotal()
+    {
+        return physics + chemistry + biology;
+    }
+
+    double getPercentage() {
+    return 100.0 * (physics + chemistry + biology) / 450.0;
+}
+
+    void printMarksSummary()
+    {
+        System.out.println("Physics: " + getPhysicsTotal());
+        System.out.println("Chemistry: " + getChemistryTotal());
+        System.out.println("Biology: " + getBiologyTotal());
+        System.out.println("Percentage: " + getPercentage());
+        System.out.println("Total: " + getAllSubjectsTotal() + "/450.0");
+    }
+
+    void printMarksDetails()
+    {
+        System.out.println("Physics : " + physics);
+        System.out.println("Chemistry : " + chemistry);
+        System.out.println("Biology : " + biology);
+    }
+}
