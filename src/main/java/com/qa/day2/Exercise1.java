25cef8ab006bf15005c6b78955c0dd6bef610eeb
Krystal <krystalsimm01@me.com>
2020-08-10 16:43:18 +0100

    Added division method

--- src/main/java/com/qa/day2/Exercise1.java
+++ src/main/java/com/qa/day2/Exercise1.java
@@ -5,6 +5,7 @@ public class Exercise1 {
         addIntegers(4, 3);
         multiplyIntegers(4, 3);
         subtractIntegers(4, 3);
+        divideIntegers(4, 12);
     }
     public static int addIntegers (int num1, int num2) {
         int result = num1 + num2;
@@ -21,6 +22,17 @@ public class Exercise1 {
     public static int subtractIntegers (int num1, int num2) {
         int result = num1 - num2;
         System.out.println(result);
+        return result;
+    }
+
+    public static double divideIntegers (double num1, double num2) {
+        double result = 0;
+        if(num1 >= num2) {
+            result = num1 / num2;
+            System.out.println(result);
+        } else {
+            System.out.println("The division cannot be performed");
+        }
         return result;
     }
 }
