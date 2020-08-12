8339373bb04e749e8809909502589751fd5e92a7
Krystal <krystalsimm01@me.com>
2020-08-12 15:41:58 +0100

    initial commit

--- src/main/java/com/qa/day3/Exercise2.java
+++ src/main/java/com/qa/day3/Exercise2.java
@@ -1,15 +1,15 @@
-/*package com.qa.day3;
+package com.qa.day3;
 
 public class Exercise2 {
     public static void main(String[] args) {
         for(int i = 100; i <= 200; i++) {
             if(i % 2 == 0){
-                System.out.println("-");
+                System.out.print("- ");
             } else {
-                System.out.println("*");
+                System.out.print("* ");
             }
             System.out.println(i);
         }
     }
 
-}*/
+}
